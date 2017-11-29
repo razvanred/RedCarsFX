29fa6bd2b00f499d9ee66a84ed505aa724305eb2
Razvan <razvanred99@gmail.com>
2017-11-29 18:41:15 +0100

    Nothing

--- src/carsFX/control/Login.java
+++ src/carsFX/control/Login.java
@@ -3,12 +3,11 @@ package carsFX.control;
 import carsFX.model.Filiale;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXRadioButton;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -16,12 +15,11 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Login implements Initializable{
+public class Login implements Initializable {
 
     @FXML
     private VBox filiali;
@@ -37,33 +35,35 @@ public class Login implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        toggleGroup=new ToggleGroup();
+        toggleGroup = new ToggleGroup();
 
-        for(Filiale f:Filiale.values()) {
-            JFXRadioButton radio=new JFXRadioButton(f.getNome());
+        for (Filiale f : Filiale.values()) {
+            JFXRadioButton radio = new JFXRadioButton(f.getNome());
             filiali.getChildren().add(radio);
             radio.setUserData(f.getPassword());
             radio.setToggleGroup(toggleGroup);
         }
 
-        if(!toggleGroup.getToggles().isEmpty()) {
+        if (!toggleGroup.getToggles().isEmpty()) {
             toggleGroup.getToggles().get(0).setSelected(true);
             passwordField.textProperty().addListener((observable, oldValue, newValue) -> somethingWrong.setText(""));
         }
     }
 
-    public void checkPasskey(ActionEvent ae){
+    public void checkPasskey(ActionEvent ae) {
 
-        if(toggleGroup.getSelectedToggle().getUserData().toString().compareTo(passwordField.getText())==0){
+        if (toggleGroup.getSelectedToggle().getUserData().toString().compareTo(passwordField.getText()) == 0) {
             try {
+
+                ((Stage)((Node)ae.getSource()).getScene().getWindow()).close();
+
                 Stage primaryStage = new Stage();
                 Parent root = FXMLLoader.load(getClass().getResource("../view/main.fxml"));
-                primaryStage.setScene(new Scene(root,550,450));
-            }catch(IOException io){
+                primaryStage.setScene(new Scene(root, 550, 450));
+            } catch (IOException io) {
                 io.printStackTrace();
             }
-        }
-        else
+        } else
             somethingWrong.setText("Password errata");
     }
 
